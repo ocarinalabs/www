7f9104ee4beb0d8cf128971c3d6858b5b8871097
faw <fawwad30@gmail.com>
2025-06-14 18:20:49 +0800

    feat(fonts): add custom font 'heming' and update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,19 @@ export default function Home() {
   return (
     <main className="relative">
       <EngravedString text="sidekick" />
-      <div className="absolute bottom-10 left-0 right-0 flex justify-center space-x-8">
-        <Link href="/robin" className="underline text-white">
-          robin
-        </Link>
-        <Link href="/labs" className="underline text-orange-500">
-          labs
-        </Link>
-        <Link href="/community" className="underline text-green-500">
-          community
-        </Link>
+      <div className="absolute bottom-10 left-0 right-0 flex flex-col items-center space-y-4">
+        <h1 className="text-5xl text-gray-500 font-heming">sidekick</h1>
+        <div className="flex justify-center space-x-8">
+          <Link href="/robin" className="underline text-white font-heming">
+            robin
+          </Link>
+          <Link href="/labs" className="underline text-orange-500 font-heming">
+            labs
+          </Link>
+          <Link href="/community" className="underline text-green-500 font-heming">
+            community
+          </Link>
+        </div>
       </div>
     </main>
   );
