b5c64230a7b9480cc9826a026f02d7b1c370ba0a
faw <fawwad30@gmail.com>
2025-08-03 02:42:58 +0800

    feat: fix favicon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -1,38 +1,47 @@
 import { ImageResponse } from "next/og";
 
-// Image metadata
 export const size = {
   width: 32,
   height: 32,
 };
 export const contentType = "image/png";
 
-// Image generation
-export default function Icon() {
+export default function Icon(props: {
+  params?: object;
+  searchParams?: { size?: string };
+}) {
+  const iconSize = props.searchParams?.size
+    ? parseInt(props.searchParams.size)
+    : 32;
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
-          fontSize: 24,
           background: "black",
           width: "100%",
           height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          color: "white",
-          fontFamily: "monospace",
         }}
       >
-        ⊨
+        <svg
+          width={iconSize * 0.6}
+          height={iconSize * 0.6}
+          viewBox="0 0 197 197"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0.300001 197V0.799987H21.6V53.3H196.5V74.6H21.6V123.2H196.5V144.5H21.6V197H0.300001Z"
+            fill="white"
+          />
+        </svg>
       </div>
     ),
-    // ImageResponse options
     {
-      // For convenience, we can re-use the exported icons size metadata
-      // config to also set the ImageResponse's width and height.
-      ...size,
+      width: iconSize,
+      height: iconSize,
     }
   );
 }
