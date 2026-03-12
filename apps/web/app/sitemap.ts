278445df5e1cc6dd1a14d83cbf551dab1b6f5a7a
faw <fawwad30@gmail.com>
2026-03-13 03:48:27 +0800

    refactor(seo): simplify sitemap to remove CMS dependency

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -1,7 +1,5 @@
 import fs from "node:fs";
-import { blog, legal } from "@repo/cms";
 import type { MetadataRoute } from "next";
-import { env } from "@/env";
 
 const appFolders = fs.readdirSync("app", { withFileTypes: true });
 const pages = appFolders
@@ -9,28 +7,18 @@ const pages = appFolders
   .filter((folder) => !folder.name.startsWith("_"))
   .filter((folder) => !folder.name.startsWith("("))
   .map((folder) => folder.name);
-const blogs = (await blog.getPosts()).map((post) => post._slug);
-const legals = (await legal.getPosts()).map((post) => post._slug);
-const protocol = env.VERCEL_PROJECT_PRODUCTION_URL?.startsWith("https")
-  ? "https"
-  : "http";
-const url = new URL(`${protocol}://${env.VERCEL_PROJECT_PRODUCTION_URL}`);
+
+const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : "https://ocarina.dev";
 
 const sitemap = async (): Promise<MetadataRoute.Sitemap> => [
   {
-    url: new URL("/", url).href,
+    url: baseUrl,
     lastModified: new Date(),
   },
   ...pages.map((page) => ({
-    url: new URL(page, url).href,
-    lastModified: new Date(),
-  })),
-  ...blogs.map((blog) => ({
-    url: new URL(`blog/${blog}`, url).href,
-    lastModified: new Date(),
-  })),
-  ...legals.map((legal) => ({
-    url: new URL(`legal/${legal}`, url).href,
+    url: `${baseUrl}/${page}`,
     lastModified: new Date(),
   })),
 ];
